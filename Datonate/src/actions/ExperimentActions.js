29283326be2ae766ee2b57a78b13705793fc0a4e
Saahil Parikh <saahilparikh97@gmail.com>
2017-11-20 04:06:57 -0500

    Assigning experiment batch to user who selects task

--- Datonate/src/actions/ExperimentActions.js
+++ Datonate/src/actions/ExperimentActions.js
@@ -49,3 +49,17 @@ const addExpToState = (dispatch, experiment) => {
     payload: experiment
   });
 };
+
+export const assignExperiment = (userID, experimentID) => {
+  return (dispatch) => {
+    var qur = "http://datonate.com:5000/api/" + userID + "/assign/" + experimentID;
+    fetch(qur)
+    .then(function(response){
+        response.text().then(function(responseText){
+          console.log(JSON.parse(responseText))
+        })
+    }).catch(function(error){
+      console.log(error);
+    });
+  };
+};
--- Datonate/src/components/ViewExperiment.js
+++ Datonate/src/components/ViewExperiment.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
-import { Card, CardSection, Button } from './common';
+import { Button } from './common';
+import {assignExperiment} from '../actions';
 
 class ViewExperiment extends Component{
+  onSelectPress() {
+    const { currentExperiment, id } = this.props;
+    this.props.assignExperiment(id, currentExperiment.id);
+  }
   render(){
-    console.log(this.props);
+
+    //console.log(this.props);
     const {currentExperiment} = this.props;
     return (
       <View>
       <Text> The Experiment ID is {currentExperiment.id}</Text>
       <Text> You will be paid {currentExperiment.price} for performing this experiment</Text>
       <Text> Short description of the experiment: {currentExperiment.description}</Text>
+      <Button onPress={this.onSelectPress.bind(this)}>
+        Start Task
+      </Button>
       </View>
     );
 
   }
 }
 
-const mapStateToProps = ({ exp }) => {
+const mapStateToProps = ({ exp, auth }) => {
   const {currentExperiment} = exp;
-  console.log(currentExperiment);
-  return {currentExperiment};
+  const {id} = auth;
+  //console.log(currentExperiment);
+  return {currentExperiment, id};
+
 };
 
-export default connect(mapStateToProps)(ViewExperiment);
+export default connect(mapStateToProps, {assignExperiment})(ViewExperiment);
