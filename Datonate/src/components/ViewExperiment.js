e6b596c1bb4f9781078c875f2ee52db91d374496
Saahil Parikh <saahilparikh97@gmail.com>
2017-11-20 22:02:41 -0500

    Files for annotation stored in state. Dashboard component ready to be set up

--- Datonate/src/components/ViewExperiment.js
+++ Datonate/src/components/ViewExperiment.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
-import { Button } from './common';
+import { Button, Card, CardSection } from './common';
 import {assignExperiment} from '../actions';
 
 class ViewExperiment extends Component{
   onSelectPress() {
+    console.log("IN");
     const { currentExperiment, id } = this.props;
     this.props.assignExperiment(id, currentExperiment.id);
   }
@@ -14,14 +15,22 @@ class ViewExperiment extends Component{
     //console.log(this.props);
     const {currentExperiment} = this.props;
     return (
-      <View>
+      <Card>
+      <CardSection>
       <Text> The Experiment ID is {currentExperiment.id}</Text>
+      </CardSection>
+      <CardSection>
       <Text> You will be paid {currentExperiment.price} for performing this experiment</Text>
+      </CardSection>
+      <CardSection>
       <Text> Short description of the experiment: {currentExperiment.description}</Text>
+      </CardSection>
+      <CardSection>
       <Button onPress={this.onSelectPress.bind(this)}>
         Start Task
       </Button>
-      </View>
+      </CardSection>
+      </Card>
     );
 
   }
