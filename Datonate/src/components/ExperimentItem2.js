b02d413529973627b5e129e5fcba3717a56eb346
Saahil Parikh <saahilparikh97@gmail.com>
2017-12-01 16:44:13 -0500

    Minor changes to Experiment Item.

--- Datonate/src/components/ExperimentItem2.js
+++ Datonate/src/components/ExperimentItem2.js
@@ -22,10 +22,6 @@ class ExperimentItem extends Component{
               <Left>
                 <Body>
                   <Text style={{ fontSize: 22, color: 'black' }}>{experiment.description}{"\n"}</Text>
-                  <Text>
-                    <Icon name="person" />
-                    <Text note style={{ fontSize: 18, color: 'black' }}>  By [Experimenter]</Text>
-                  </Text>
                 </Body>
               </Left>
             </CardItem>
@@ -33,8 +29,8 @@ class ExperimentItem extends Component{
               <Left>
                   <Text>
                     <Icon active name="time" style={{ fontSize: 19, color: '#FFCC00' }} />
-                    <Text> </Text>
-                    <Text style={{ fontSize: 18, color: 'black' }}>11/28</Text>
+
+                    <Text style={{ fontSize: 18, color: 'black' }}>{ experiment.due } </Text>
                   </Text>
               </Left>
               <Body>
